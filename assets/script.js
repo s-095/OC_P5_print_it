d704ff8ad598f72a8f332d150517fc0496b9e561
Sab <s.svrn9@proton.me>
2024-09-05 19:49:38 +0200

    renommages des variables pour plus de clarté

--- assets/script.js
+++ assets/script.js
@@ -23,37 +23,37 @@ const arrowLeft = document.querySelector(".arrow_left")
 const arrowRight = document.querySelector(".arrow_right")
 const dots = document.querySelectorAll(".dot")
 
-let i = 0
+let slideIndex = 0
 
 function updateSlide() {
-	slideImage.src = slides[i].image
-	slideTagline.innerHTML = slides[i].tagLine
+	slideImage.src = slides[slideIndex].image
+	slideTagline.innerHTML = slides[slideIndex].tagLine
 	updateDots();
 }
 
 function slidesEvent() {
 	arrowLeft.addEventListener("click", () => {
-		if (i === 0) {
-			i = slides.length - 1
+		if (slideIndex === 0) {
+			slideIndex = slides.length - 1
 		} else {
-			i--
+			slideIndex--
 		}
 		updateSlide()
 		console.log("clic flèche gauche!")
 	})
 
 	arrowRight.addEventListener("click", () => {
-		if (i === slides.length - 1) {
-			i = 0
+		if (slideIndex === slides.length - 1) {
+			slideIndex = 0
 		} else {
-			i++
+			slideIndex++
 		}
 		updateSlide()
 		console.log("clic flèche droite!")
 	})
-	for (let dotNumber = 0; dotNumber < dots.length; dotNumber++) {
-		dots[dotNumber].addEventListener("click", () => {
-			i = dotNumber
+	for (let dotIndex = 0; dotIndex < dots.length; dotIndex++) {
+		dots[dotIndex].addEventListener("click", () => {
+			slideIndex = dotIndex
 			updateSlide()
 			console.log("clic bullet point!")
 		})
@@ -61,11 +61,11 @@ function slidesEvent() {
 }
 
 function updateDots() {
-	for (dotNumber = 0; dotNumber < dots.length; dotNumber++) {
-		if (dotNumber === i) {
-			dots[dotNumber].classList.add("dot_selected");
+	for (dotIndex = 0; dotIndex < dots.length; dotIndex++) {
+		if (dotIndex === slideIndex) {
+			dots[dotIndex].classList.add("dot_selected");
 		} else {
-			dots[dotNumber].classList.remove("dot_selected");
+			dots[dotIndex].classList.remove("dot_selected");
 		}
 	}
 }
